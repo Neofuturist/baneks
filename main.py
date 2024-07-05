4fbb1fe393565e7f990522c0f2fd759712992624
Neofuturist <e.derevlev@yandex.ru>
2024-07-05 16:52:46 +0300

    bug fix, added number of anek

--- main.py
+++ main.py
@@ -12,34 +12,43 @@ bot = telebot.TeleBot('7129810701:AAEmpkOutHO5t3d6qOmvA3i4ZtGcI2aMWTw')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Случайный анек")
     markup.add(btn1)
-    bot.send_message(message.from_user.id, "Привет! Нажми на кнопку, чтобы прочитать анек!", reply_markup=markup)
+    bot.send_message(message.from_user.id, "Нажми на кнопку, чтобы прочитать анек. \nВведи номер анека, чтобы прочитать его.", reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-
     if message.text == 'Случайный анек':
         anek = get_anek()
-        bot.send_message(message.from_user.id, anek)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Случайный анек")
+        markup.add(btn1)
+        bot.send_message(message.from_user.id, anek, parse_mode='Markdown', disable_web_page_preview=True, reply_markup=markup)
+    elif message.text.isdigit():
+        anek = get_anek(message.text)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Случайный анек")
+        markup.add(btn1)
+        bot.send_message(message.from_user.id, anek, parse_mode='Markdown', disable_web_page_preview=True, reply_markup=markup)
+    else:
+        start(message)
 
 
-def get_anek():
+def get_anek(number=None):
     conn = http.client.HTTPSConnection("baneks.ru")
     anekId = randrange(1, 1142)
+    if number:
+        anekId = int(number)
     conn.request("GET", "/" + str(anekId))
     response = conn.getresponse()
     print(response.status, response.reason)
-    # print(response.read().decode("utf-8"))
     response_str = response.read().decode("utf-8")
-    start_number = str(response_str[response_str.find("<meta name=\"title\" content=\"") + 28: len(response_str)])
-    end_number = start_number.split("\"")[0]
+    end_number = "Анекдот [#" + str(anekId) + "](baneks.ru/" + str(anekId) + ")"
     print("number: " + end_number)
     start_anek = str(response_str[response_str.find("<meta name=\"description\" content=\"") + 34: len(response_str)])
-    end_anek = start_anek.split("\"")[0]
+    end_anek = start_anek.split("\">")[0]
     print("anek: " + end_anek)
     conn.close()
     return end_number + "\n\n" + end_anek
